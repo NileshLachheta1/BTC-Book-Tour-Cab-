927251dc1bd66c1ea56db57189b1045addaa351f
xyz <xyz@gmail.com>
2023-09-07 22:02:50 +0530

    Updated BTC

--- public/js/navbar.js
+++
@@ -1,5 +0,0 @@
-var normal_user_profile_referenece = document.getElementById("normal-user-profile-reference");
-
-// normal_user_profile_referenece.addEventListener('click',()=>{
-//     window.location.href=
-// });
--- public/js/normal_user_profile.js
+++
@@ -1,5 +0,0 @@
-// var user_dashboard_button = document.getElementById("user-dashboard-button");
-
-// user_dashboard_button.addEventListener("click",()=>{
-//     window.location.href="long_term_user_dashboard";
-// })
